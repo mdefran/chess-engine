a2fbd5e03c536a48ffff2d9d62cec9e98e579fe5
Matthew DeFranceschi <matthew.defranceschi@gmail.com>
2023-07-24 22:57:25 -0400

    initial pawn move gen implementation

--- chess.cpp
+++ chess.cpp
@@ -81,6 +81,10 @@ const Bitboard RANK_6 = 0x0000FF0000000000ULL;
 const Bitboard RANK_7 = 0x00FF000000000000ULL;
 const Bitboard RANK_8 = 0xFF00000000000000ULL;
 
+// General masks
+const Bitboard EMPTY = 0ULL;
+const Bitboard UNIVERSE = 0xFFFFFFFFFFFFFFFFULL;
+
 // Set a bit at a given position to 1
 #define SET_BIT(bitboard, bit) ((bitboard) |= (1ULL << bit))
 // Set a bit at a given position to 0
@@ -100,6 +104,7 @@ inline int POP_LSB(Bitboard &bitboard) {
     bitboard &= bitboard - 1;
     return index;
 }
+#define BITBOARD(square) ((1ULL << square))
 
 // Directional helper functions with boundary validation
 constexpr Bitboard north(Bitboard bitboard) { return (bitboard & ~RANK_8) << 8; }
@@ -147,7 +152,13 @@ struct Chessboard {
     
     // Declare lookup tables for leaping pieces
     Bitboard knightAttacks[64], kingAttacks[64], whitePawnAttacks[64], blackPawnAttacks[64];
+
+    // Move lists for move generation
     MovesList pseudoLegalMoves, legalMoves;
+
+    // Variable to store double pawn moves that could enable an en passant
+    short enPassant;
+    bool whiteQueenCastle, whiteKingCastle, blackQueenCastle, blackKingCastle;
 
     Chessboard() {
         // Initialize the bitboards to match the piece's positions at the start of the game
@@ -171,20 +182,107 @@ struct Chessboard {
         
         // Generate attack map lookup tables for knights and kings
         for (int square = 0; square < 64; square++) {
-            Bitboard fromSquare = 1ULL << square;
+            Bitboard fromSquare = BITBOARD(square);
 
             kingAttacks[square] = north(fromSquare) | northeast(fromSquare) | east(fromSquare) | 
                                   southeast(fromSquare) | south(fromSquare) | southwest(fromSquare) |
                                   west(fromSquare) | northwest(fromSquare);
 
+            // Edit boundary checking to within fromSquare to bitboard conversion if causing errors
             knightAttacks[square] = (((fromSquare << 6) | (fromSquare >> 10)) & ~(FILE_A | FILE_B)) |
                                     (((fromSquare << 15) | (fromSquare >> 17)) & ~(FILE_A)) |
                                     (((fromSquare << 17) | (fromSquare >> 15)) & ~(FILE_H)) |
                                     (((fromSquare << 10) | (fromSquare >> 6)) & ~(FILE_G | FILE_H));
 
-            whitePawnAttacks[square] = ((fromSquare & RANK_2) != 0) ? north(fromSquare) | (fromSquare << 16) : (north(fromSquare) & ~(RANK_8));
-            blackPawnAttacks[square] = ((fromSquare & RANK_7) != 0) ? south(fromSquare) | (fromSquare >> 16) : (south(fromSquare) & ~(RANK_1));
+            //whitePawnAttacks[square] = ((fromSquare & RANK_2) != 0) ? north(fromSquare) | (fromSquare << 16) : (north(fromSquare) & ~(RANK_8));
+            //blackPawnAttacks[square] = ((fromSquare & RANK_7) != 0) ? south(fromSquare) | (fromSquare >> 16) : (south(fromSquare) & ~(RANK_1));
+            whitePawnAttacks[square] = north(fromSquare);
+            blackPawnAttacks[square] = south(fromSquare);
         }
+    }
+
+    // Determine if the opponent has a piece on a given square and return true if so
+    bool enemyAt(short square) {
+        Bitboard enemyPieces = (turn == White) ? blackPieces : whitePieces;
+        return enemyPieces & BITBOARD(square);
+    }
+
+    // Efficiently store a move in the pseudo legal moves list
+    void addMove(short fromSquare, short toSquare) {
+        Move move = fromSquare | (toSquare << 6);
+        pseudoLegalMoves.push_back(move);
+    }
+
+    // Generate pseudo legal king moves for the current player using the attack maps
+    void generateKingMoves() {
+        short fromSquare = (turn == White) ? POP_LSB(whiteKing) : POP_LSB(blackKing);
+        Bitboard toSquares = kingAttacks[fromSquare];
+        while (toSquares != 0) {
+            addMove(fromSquare, POP_LSB(toSquares));
+        }
+    }
+
+    // Generate pseudo legal knight moves for the current player using the attack maps
+    void generateKnightMoves() {
+        Bitboard fromSquares = (turn == White) ? whiteKnights : blackKnights;
+        while (fromSquares != 0) {
+            short fromSquare = POP_LSB(fromSquares);
+            Bitboard toSquares = knightAttacks[fromSquare];
+            while (toSquares != 0) {
+                addMove(fromSquare, POP_LSB(toSquares));
+            }
+        }
+    }
+
+    // Generate pseudo legal pawn moves for the current player 
+    void generatePawnMoves() {
+        Bitboard fromSquares = (turn == White) ? whitePawns : blackPawns;
+        if (turn == White) {
+            while (fromSquares != 0) {
+                Bitboard fromSquare = BITBOARD(POP_LSB(fromSquares));
+                Bitboard toSquares = whitePawnAttacks[fromSquare];
+                // If there is an enemy piece in capture range, add the capture to the possible moves
+                if ((northeast(fromSquare) & blackPieces) != 0)
+                    toSquares |= (northeast(fromSquare));
+                if ((northwest(fromSquare) & blackPieces) != 0)
+                    toSquares |= (northwest(fromSquare));
+                // If it is the pawn's first move and there are no pieces in the way, add the double advance to the possible moves
+                if ((fromSquare & RANK_2) != 0 && (((fromSquare << 8) | (fromSquare << 16)) & (whitePieces | blackPieces)) == 0) {
+                    toSquares |= (fromSquare << 16);
+                    // Store the move as a possible en passant
+                    enPassant = fromSquare;
+                }
+            }
+        }
+        else {
+            while (fromSquares != 0) {
+                Bitboard fromSquare = BITBOARD(POP_LSB(fromSquares));
+                Bitboard toSquares = blackPawnAttacks[fromSquare];
+                // If there is an enemy piece in capture range, add the capture to the possible moves
+                if ((southeast(fromSquare) & whitePieces) != 0)
+                    toSquares |= (southeast(fromSquare));
+                if ((southwest(fromSquare) & whitePieces) != 0)
+                    toSquares |= (southwest(fromSquare));
+                // If it is the pawn's first move and there are no pieces in the way, add the double advance to the possible moves
+                if ((fromSquare & RANK_2) != 0 && (((fromSquare >> 8) | (fromSquare >> 16)) & (whitePieces | blackPieces)) == 0) {
+                    toSquares |= (fromSquare >> 16);
+                    // Store the move as a possible en passant
+                    enPassant = fromSquare;
+                }
+            }
+        }
+    }
+
+    void printPseudoMoves() {
+        for (const auto &move : pseudoLegalMoves) {
+            short from = move & 0x3F;
+            short to = (move >> 6) & 0x3F;
+            std::cout << static_cast<Square>(from) << " " << static_cast<Square>(to) << std::endl;
+        }
+    }
+
+    void generateLegalMoves() {
+
     }
 };
 
