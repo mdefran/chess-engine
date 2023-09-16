82138eed9100ad87df82fe0879a98676c0b410e1
Matthew DeFranceschi <matthew.defranceschi@gmail.com>
2023-09-16 14:15:08 -0400

    reformatted move gen

--- move_generation.cpp
+++ move_generation.cpp
@@ -1,4 +1,3 @@
-#include <unordered_map>
 #include "chessboard.h"
 #include "move.h"
 #include "bitboard.h"
@@ -6,7 +5,7 @@
 #include "board_visualization.h"
 
 // Declare lookup tables for leaping pieces
-Bitboard knightAttacks[64], kingAttacks[64], whitePawnAttacks[64], blackPawnAttacks[64], whitePawnAdvances[64], blackPawnAdvances[64], whitePawnCaptures[64], blackPawnCaptures[64];
+Bitboard knightAttacks[64], kingAttacks[64], whitePawnAdvances[64], blackPawnAdvances[64], whitePawnCaptures[64], blackPawnCaptures[64];
 // Declare sliding piece run-time attack boards
 Bitboard rookAttacks, bishopAttacks;
 
@@ -29,9 +28,7 @@ void initializeLookupTables() {
                                 (((fromSquare & ~(FILE_A | RANK_7 | RANK_8)) << 17) | ((fromSquare & ~(FILE_A | RANK_1 | RANK_2)) >> 15)) |
                                 (((fromSquare & ~(FILE_A | FILE_B | RANK_8)) << 10) | ((fromSquare & ~(FILE_A | FILE_B | RANK_1)) >> 6));
 
-        // Pawns can advance forward (captures and special moves handled in generation)
-        // whitePawnAttacks[square] = north(fromSquare);
-        // blackPawnAttacks[square] = south(fromSquare);
+        // Pawns can advance forward or capture diagonally forward
         whitePawnAdvances[square] = north(fromSquare);
         blackPawnAdvances[square] = south(fromSquare);
         whitePawnCaptures[square] = northeast(fromSquare) | northwest(fromSquare);
@@ -64,31 +61,50 @@ void pushPseudoLegalPromotion(Chessboard &chessboard, Square fromSquare, Square 
     }
 }
 
-void generateKingMoves(Chessboard &chessboard) {
-    Square fromSquare = (chessboard.turn == White) ? static_cast<Square>(POP_LSB(chessboard.whiteKing)) : static_cast<Square>(POP_LSB(chessboard.blackKing));
-    Bitboard toSquares = kingAttacks[fromSquare];
-    
-    Move move;
-    // Check for pseudo legal castling
-    if (chessboard.turn == White) {
-        if (chessboard.whiteKingCastle == true && (chessboard.allPieces & (0x6ULL)) == 0)
-            chessboard.pseudoLegalMoves.push_back(Move(Square::e1, Square::g1, Move::KingCastle));
-        if (chessboard.whiteQueenCastle == true && (chessboard.allPieces & (0x70ULL)) == 0)
-            chessboard.pseudoLegalMoves.push_back(Move(Square::e1, Square::c1, Move::QueenCastle));
-    } else {
-        if (chessboard.blackKingCastle == true && (chessboard.allPieces & (0x600000000000000ULL)) == 0)
-            chessboard.pseudoLegalMoves.push_back(Move(Square::e8, Square::g8, Move::KingCastle));
-        if (chessboard.blackQueenCastle == true && (chessboard.allPieces & (0x7000000000000000) == 0))
-            chessboard.pseudoLegalMoves.push_back(Move(Square::e8, Square::c8, Move::QueenCastle));
-    }
+void generatePawnMoves(Chessboard &chessboard) {
+    Bitboard fromSquares = (chessboard.turn == White) ? chessboard.whitePawns : chessboard.blackPawns;
+    Bitboard toSquares;
 
-    while (toSquares != 0) {
-        Square toSquare = static_cast<Square>(POP_LSB(toSquares));
-        pushPseudoLegalMove(chessboard, fromSquare, toSquare);
+    while (fromSquares != 0) {
+        Square fromSquare = static_cast<Square>(POP_LSB(fromSquares));
+
+        // Add normal advances
+        Bitboard toSquares = (chessboard.turn == White) ? whitePawnAdvances[fromSquare] : blackPawnAdvances[fromSquare];
+
+        // Add double advances when origin square is starting position and there are no pieces directly in front
+        if (chessboard.turn == White && (BITBOARD(fromSquare) & RANK_2) && ((((BITBOARD(fromSquare) << 8) | (BITBOARD(fromSquare) << 16)) & chessboard.allPieces) == 0))
+            chessboard.pseudoLegalMoves.push_back(Move(fromSquare, static_cast<Square>(GET_LSB(BITBOARD(fromSquare) << 16)), Move::DoublePawnPush));
+        if (chessboard.turn == Black && (BITBOARD(fromSquare) & RANK_7) && ((((BITBOARD(fromSquare) >> 8) | (BITBOARD(fromSquare) >> 16)) & chessboard.allPieces) == 0))
+            chessboard.pseudoLegalMoves.push_back(Move(fromSquare, static_cast<Square>(GET_LSB(BITBOARD(fromSquare) >> 16)), Move::DoublePawnPush));
+
+        // Add diagonal captures
+        Bitboard potentialCaptures = (chessboard.turn == White) ? whitePawnCaptures[fromSquare] : blackPawnCaptures[fromSquare];
+        while (potentialCaptures != 0) {
+            Square potentialCapture = static_cast<Square>(POP_LSB(potentialCaptures));
+            if (BITBOARD(potentialCapture) & ((chessboard.turn == White) ? chessboard.blackPieces : chessboard.whitePieces))
+                toSquares |= BITBOARD(potentialCapture);
+        }
+
+        // Add en passant
+        if (chessboard.enPassant == east(BITBOARD(fromSquare)))
+            chessboard.pseudoLegalMoves.push_back(Move(BITBOARD(fromSquare), ((chessboard.turn = White) ? northeast(BITBOARD(fromSquare)) : southeast(BITBOARD(fromSquare))), Move::EnPassant));
+        if (chessboard.enPassant == west(BITBOARD(fromSquare)))
+            chessboard.pseudoLegalMoves.push_back(Move(BITBOARD(fromSquare), ((chessboard.turn = White) ? northwest(BITBOARD(fromSquare)) : southwest(BITBOARD(fromSquare))), Move::EnPassant));
+
+        // Push pseudo legal moves
+        Move move;
+        while (toSquares != 0) {
+            Square toSquare = static_cast<Square>(POP_LSB(toSquares));
+            // If the pawn has reached the end of the board, add promotions
+            if (BITBOARD(toSquare) & (RANK_1 | RANK_8))
+                pushPseudoLegalPromotion(chessboard, fromSquare, toSquare);
+            // Otherwise, add normal moves
+            else
+                pushPseudoLegalMove(chessboard, fromSquare, toSquare);
+        }
     }
 }
 
-// Generate pseudo legal knight moves for the current player using the attack maps
 void generateKnightMoves(Chessboard &chessboard) {
     Bitboard fromSquares = (chessboard.turn == White) ? chessboard.whiteKnights : chessboard.blackKnights;
     while (fromSquares != 0) {
@@ -307,9 +323,6 @@ void generateQueenMoves(Chessboard &chessboard) {
                 break;
         }
 
-        printBitboard(fromSquares);
-        printBitboard(toSquares);
-
         // Push pseudo legal move
         Move move;
         while (toSquares != 0) {
@@ -319,73 +332,51 @@ void generateQueenMoves(Chessboard &chessboard) {
     }
 }
 
-void generatePawnMoves(Chessboard &chessboard) {
-    Bitboard fromSquares = (chessboard.turn == White) ? chessboard.whitePawns : chessboard.blackPawns;
-    Bitboard toSquares;
+void generateKingMoves(Chessboard &chessboard) {
+    Square fromSquare = (chessboard.turn == White) ? static_cast<Square>(POP_LSB(chessboard.whiteKing)) : static_cast<Square>(POP_LSB(chessboard.blackKing));
+    Bitboard toSquares = kingAttacks[fromSquare];
+    
+    Move move;
+    // Check for pseudo legal castling
+    if (chessboard.turn == White) {
+        if (chessboard.whiteKingCastle == true && (chessboard.allPieces & (0x6ULL)) == 0)
+            chessboard.pseudoLegalMoves.push_back(Move(Square::e1, Square::g1, Move::KingCastle));
+        if (chessboard.whiteQueenCastle == true && (chessboard.allPieces & (0x70ULL)) == 0)
+            chessboard.pseudoLegalMoves.push_back(Move(Square::e1, Square::c1, Move::QueenCastle));
+    } else {
+        if (chessboard.blackKingCastle == true && (chessboard.allPieces & (0x600000000000000ULL)) == 0)
+            chessboard.pseudoLegalMoves.push_back(Move(Square::e8, Square::g8, Move::KingCastle));
+        if (chessboard.blackQueenCastle == true && (chessboard.allPieces & (0x7000000000000000) == 0))
+            chessboard.pseudoLegalMoves.push_back(Move(Square::e8, Square::c8, Move::QueenCastle));
+    }
 
-    while (fromSquares != 0) {
-        Square fromSquare = static_cast<Square>(POP_LSB(fromSquares));
-
-        // Add normal advances
-        Bitboard toSquares = (chessboard.turn == White) ? whitePawnAdvances[fromSquare] : blackPawnAdvances[fromSquare];
-
-        // Add double advances when origin square is starting position and there are no pieces directly in front
-        if (chessboard.turn == White && (BITBOARD(fromSquare) & RANK_2) && ((((BITBOARD(fromSquare) << 8) | (BITBOARD(fromSquare) << 16)) & chessboard.allPieces) == 0))
-            chessboard.pseudoLegalMoves.push_back(Move(fromSquare, static_cast<Square>(GET_LSB(BITBOARD(fromSquare) << 16)), Move::DoublePawnPush));
-        if (chessboard.turn == Black && (BITBOARD(fromSquare) & RANK_7) && ((((BITBOARD(fromSquare) >> 8) | (BITBOARD(fromSquare) >> 16)) & chessboard.allPieces) == 0))
-            chessboard.pseudoLegalMoves.push_back(Move(fromSquare, static_cast<Square>(GET_LSB(BITBOARD(fromSquare) >> 16)), Move::DoublePawnPush));
-
-        // Add diagonal captures
-        Bitboard potentialCaptures = (chessboard.turn == White) ? whitePawnCaptures[fromSquare] : blackPawnCaptures[fromSquare];
-        while (potentialCaptures != 0) {
-            Square potentialCapture = static_cast<Square>(POP_LSB(potentialCaptures));
-            if (BITBOARD(potentialCapture) & ((chessboard.turn == White) ? chessboard.blackPieces : chessboard.whitePieces))
-                toSquares |= BITBOARD(potentialCapture);
-        }
-
-        // Add en passant
-        if (chessboard.enPassant == east(BITBOARD(fromSquare)))
-            chessboard.pseudoLegalMoves.push_back(Move(BITBOARD(fromSquare), ((chessboard.turn = White) ? northeast(BITBOARD(fromSquare)) : southeast(BITBOARD(fromSquare))), Move::EnPassant));
-        if (chessboard.enPassant == west(BITBOARD(fromSquare)))
-            chessboard.pseudoLegalMoves.push_back(Move(BITBOARD(fromSquare), ((chessboard.turn = White) ? northwest(BITBOARD(fromSquare)) : southwest(BITBOARD(fromSquare))), Move::EnPassant));
-
-        // Push pseudo legal moves
-        Move move;
-        while (toSquares != 0) {
-            Square toSquare = static_cast<Square>(POP_LSB(toSquares));
-            // If the pawn has reached the end of the board, add promotions
-            if (BITBOARD(toSquare) & (RANK_1 | RANK_8))
-                pushPseudoLegalPromotion(chessboard, fromSquare, toSquare);
-            // Otherwise, add normal moves
-            else
-                pushPseudoLegalMove(chessboard, fromSquare, toSquare);
-        }
+    while (toSquares != 0) {
+        Square toSquare = static_cast<Square>(POP_LSB(toSquares));
+        pushPseudoLegalMove(chessboard, fromSquare, toSquare);
     }
 }
 
-/*
 void Chessboard::generatePseudoLegalMoves() {
-    //generatePawnMoves(*this);
-    //generateKnightMoves(*this);
-    //generateRookMoves(*this);
-    //generateBishopMoves(*this);
-    //generateQueenMoves(*this);
-    //generateKingMoves(*this);
+    generatePawnMoves(*this);
+    generateKnightMoves(*this);
+    generateRookMoves(*this);
+    generateBishopMoves(*this);
+    generateQueenMoves(*this);
+    generateKingMoves(*this);
 }
-
-MoveList Chessboard::generateLegalMoves() {
-    MoveList pseudoLegalMoves = generatePseudoLegalMoves();
-}
-*/
 
 int main () {
     Chessboard chessboard;
     initializeLookupTables();
-    generatePawnMoves(chessboard);
+    chessboard.generatePseudoLegalMoves();
 
     while (!chessboard.pseudoLegalMoves.empty()) {
         Move move = chessboard.pseudoLegalMoves.back();
         move.printMove();
+        printBitboard(BITBOARD(move.getFromSquare()));
+        printf("\n");
+        printBitboard(BITBOARD(move.getToSquare()));
+        printf("\n");
         chessboard.pseudoLegalMoves.pop_back();
     }
 
